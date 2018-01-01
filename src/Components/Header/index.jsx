3a22360ad852279c8132dd052119ba43f3b51282
A.Alekseev <angelpsymail@gmail.com>
2018-01-01 21:33:44 +0500

    add base layout of header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './index.css';
+
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='b-header'>
+                <Toolbar
+                    style={{
+                        backgroundColor: this.props.muiTheme.palette.bgTertiary,
+                    }}>
+                    <ToolbarGroup>
+                        <ToolbarTitle text={'Header (breadcrumbs)'}/>
+                        {/* TODO: Хлебные крошки выделенного таймера + название /
+                        данные по таймеру по время работы таймера (? возможно будет достаточно в footer`е) */}
+                    </ToolbarGroup>
+                </Toolbar>
+            </div>
+        );
+    }
+}
+
+export default muiThemeable()(Header);
--- src/Layout/BaseLayout/index.jsx
+++ src/Layout/BaseLayout/index.jsx
@@ -3,6 +3,7 @@ import './index.css';
 
 import muiThemeable from 'material-ui/styles/muiThemeable';
 import AppToolbar from '../../Components/Topbar';
+import Header from '../../Components/Header';
 
 class BaseLayout extends Component {
     render() {
@@ -10,8 +11,12 @@ class BaseLayout extends Component {
             <div className='l-base'
                  style={{
                      backgroundColor: this.props.muiTheme.palette.bgPrimary,
+                     color: this.props.muiTheme.palette.textPrimary,
                  }}>
                 <AppToolbar />
+                <main className="l-base__main">
+                    <Header />
+                </main>
             </div>
         );
     }
