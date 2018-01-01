855e3d989daa0695a0ac5e39aa0adc50ddaeb24f
A.Alekseev <angelpsymail@gmail.com>
2018-01-01 20:19:02 +0500

    add base scheme_component

---
+++ src/__scheme_component__/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class AppComponent extends Component {
+    render() {
+        return (
+            <div>AppComponent</div>
+        );
+    }
+}
+
+export default AppComponent;
