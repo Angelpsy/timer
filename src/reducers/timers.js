19c5cb4bff1cd5042f1e797e12448fdb0091e4fe
A.Alekseev <angelpsymail@gmail.com>
2018-01-17 12:18:49 +0500

    refactoring reducer of timers (add reducer of solo timer)

--- src/reducers/timers.js
+++ src/reducers/timers.js
@@ -1,99 +1,20 @@
 import {ACTIONS} from '../constants/actions';
-import nanoid from 'nanoid';
-
-import {timers as _timers} from '../store/TestData';
-
-// TODO: разобраться с группами таймеров
-/**
- * @param {Object} timer
- * @return {{left, state: string}}
- */
-function playTimer(timer) {
-    return {
-        ...timer,
-        state: 'play',
-    }
-}
-
-// TODO: разобраться с группами таймеров
-/**
- * @param {Object} timer
- * @return {{left, state: string}}
- */
-function stopTimer(timer) {
-    return {
-        ...timer,
-        left: timer.value,
-        state: 'stop',
-    }
-}
+import timer from './timer';
 
 const timers = (state = [], action) => {
     switch (action.type) {
         case ACTIONS.GET_TIMERS:
-            return action.payload.timers.map(timer => {
-                return timer.state === 'play' ?
-                    {
-                        ...timer,
-                        state: 'pause',
-                    }
-                    : timer
-            });
+            return action.payload.timers.map(item => timer(item, action));
         case ACTIONS.ADD_TIMER:
-            return [
-                ...state,
-                {
-                    id: nanoid(),
-                    order: 0,
-                    title: action.payload.title,
-                    description: action.payload.description,
-                    childTimers: null,
-                    value: action.payload.value,
-                    left: action.payload.value,
-                    state: 'stop',
-                }
-            ];
+            return [ ...state, timer(null, action) ];
         case ACTIONS.PLAY_TIMER:
-            return state.map((timer) => {
-                if (action.payload.id === timer.id && timer.state !== 'play') {
-                    return playTimer(timer);
-                } else {
-                    return timer;
-                }
-            });
+            return state.map((item) => timer(item, action));
         case ACTIONS.PAUSE_TIMER:
-            return state.map((timer) => {
-                if (action.payload.id === timer.id && timer.state !== 'pause') {
-                    return {
-                        ...timer,
-                        state: 'pause',
-                    }
-                } else {
-                    return timer;
-                }
-            });
+            return state.map((item) => timer(item, action));
         case ACTIONS.STOP_TIMER:
-            return state.map((timer) => {
-                if (action.payload.id === timer.id && timer.state !== 'stop') {
-                    return stopTimer(timer);
-                } else {
-                    return timer;
-                }
-            });
+            return state.map((item) => timer(item, action));
         case ACTIONS.TICK:
-            let newState = state.slice();
-                newState = newState.map((timer) => {
-                    if (timer.id === action.payload.id) {
-                        return {
-                            ...timer,
-                            left: timer.left - 1,
-                        }
-                    }
-
-                    return timer;
-                });
-
-            return newState;
+            return state.map((item) => timer(item, action));
         default:
             return state
     }
